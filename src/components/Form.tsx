3f76d6066b1857bd080990d1401cb3eaf6c7f2a9
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-06-08 17:16:09 +0900

    API API

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react"
 
 const Form = () =>{
-    const [city, setCity] = useState("")
+    const [city, setCity] = useState<string>("")
+
+    const getWeather = (e: any) => {
+        e.preventDefault()
+        fetch("")
+        .then(res => console.log(res))
+    }
 
     return(
     <form>
         <input type="text" name="city" placeholder="都市名" onChange={ e => setCity(e.target.value) }/>
-        {city}
-        <button type="submit">Get Weather</button>
+        <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
     )
 }
