485555a38ade687dda87df4bc43cd4d00996c86e
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-06-10 12:47:41 +0900

    Appにfunctionを移動(One-way Data Flow)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
 import Title from "./components/Title"
 import Form from "./components/Form"
 import Results from "./components/Results"
 
 const App  = () => {
+  const [city, setCity] = useState<string>("")
+
+  const getWeather = (e: any) => {
+      e.preventDefault()
+      fetch("")
+      .then(res => console.log(res))
+  }
+
   return (
     <div>
       <Title/>
-      <Form/>
+      <Form setCity={setCity} getWeather={getWeather} />
       <Results/>
     </div>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,13 @@
-import { useState } from "react"
+type FormProps = {
+    setCity:React.Dispatch<React.SetStateAction<string>>
+    getWeather: (e: any) => void
+}
 
-const Form = () =>{
-    const [city, setCity] = useState<string>("")
-
-    const getWeather = (e: any) => {
-        e.preventDefault()
-        fetch("")
-        .then(res => console.log(res))
-    }
-
-    return(
+const Form = (props: FormProps) =>{
+     return(
     <form>
-        <input type="text" name="city" placeholder="都市名" onChange={ e => setCity(e.target.value) }/>
-        <button type="submit" onClick={getWeather}>Get Weather</button>
+        <input type="text" name="city" placeholder="都市名" onChange={ e => props.setCity(e.target.value) }/>
+        <button type="submit" onClick={props.getWeather}>Get Weather</button>
     </form>
     )
 }
