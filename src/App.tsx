7c51a47cc8ec90097cd76f3683d79b76d3c7b115
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-06-16 22:56:51 +0900

    必要なデータを出力

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App  = () => {
 
   const getWeather = (e) => {
       e.preventDefault()
-      fetch("")
+      fetch(``)
       .then(res => res.json())
       .then(data => {
         setResults({
