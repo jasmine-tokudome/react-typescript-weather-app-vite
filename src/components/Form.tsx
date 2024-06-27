0d5dbad2d842ec7ceef4e873603e030000f512d5
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-06-27 22:07:04 +0900

    onClickイベント追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,14 +5,14 @@ const Form = () => {
 
     const getWeather = () => {
         fetch("")
-        .then(res = > )
+        .then(res => console.log(res) )
     }
 
     return(
         <form>
             <input type="text" name="city" placeholder="都市名" onChange={e=> setCity(e.target.value)}/>
             {city}
-            <button type="submit">Get Weather</button>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     )
 }
