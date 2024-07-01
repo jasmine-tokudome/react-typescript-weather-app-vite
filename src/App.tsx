bcfe13925bba8fe5b73ff9c72ad5ec7c2a828e7e
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-07-01 17:10:41 +0900

    appで気象データを取得する

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react" 
 import Title from "./components/Title"
 import Form from "./components/Form"
 import Results from "./components/Results"
 
 const App = () =>{
+  const [city, setCity] = useState("")
+
+    const getWeather = (e: any) => {
+        e.preventDefault
+        fetch("")
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
   return(
     <div>
     <Title/>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,11 @@
 import { useState } from "react"
 
-const Form = () => {
-    const [city, setCity] = useState("")
-
-    const getWeather = (e: any) => {
-        e.preventDefault
-        fetch("")
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
+const Form = (props) => {
 
     return(
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e=> setCity(e.target.value)}/>
-            {city}
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e=> props.setCity(e.target.value)}/>
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     )
 }
