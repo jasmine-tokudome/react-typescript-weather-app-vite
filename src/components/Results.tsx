13ac62f627774c1401ce7bde0d978c94acfa9d50
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2024-07-07 22:20:07 +0900

    出力するデータを揃える

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,9 @@
 const Results =  (props) => {
     return(
             {props.results.country && <div>props.results.country</div>}
+            {props.results.cityName && <div>props.results.cityName</div>}
+            {props.results.temperature && <div>props.results.temperature<span>℃</span></div>}
+            {props.results.conditionText && <div><img src={props.results.icon}<span>{props.results.conditionText}</span></div>}
     )
 }
 
